fdafab0a9d507a653a13681a0fe0ec758660a66f
cl.lee <cl.lee.ofz@gmail.com>
2023-02-17 18:57:55 +0000

    updated function to display text for Questions section

--- index.mjs
+++ index.mjs
@@ -18,59 +18,67 @@ function init() {}
 // ***PSEUDO CODES***
 // inquirer for user's input
 // inquirer
-let { title, description, installation, usage, contribution, tests, license, github, emailAddress } =
-  await inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "What is the project's title?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Please enter a description of the project",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Please enter the installation instructions",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Please enter the usage instructions",
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "Please enter the contribution guidelines",
-    },
-    {
-      type: "input",
-      name: "tests",
-      message: "Please enter the test instructions",
-    },
-    {
-        type: "checkbox",
-        name: "license",
-        message: "Select the application's license",
-        choices: [
-            { name: "MIT", value: "mit" },
-            { name: "Mozilla Public License 2.0", value: "mozilla" },
-        ],
-    },
-    {
-      type: "input",
-      name: "github",
-      message: "Please enter the your GitHub username (for contact)",
-    },
-    {
-      type: "input",
-      name: "emailAddress",
-      message: "Please enter the your email address (for contact)",
-    },
-
-  ]);
+let {
+  title,
+  description,
+  installation,
+  usage,
+  contribution,
+  tests,
+  license,
+  githubUsername,
+  emailAddress,
+} = await inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project's title?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description of the project",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please enter the installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter the usage instructions",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please enter the contribution guidelines",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Please enter the test instructions",
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Select the application's license",
+    choices: [
+      { name: "MIT", value: "mit" },
+      { name: "Mozilla Public License 2.0", value: "mozilla" },
+    ],
+  },
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "Please enter the your GitHub username (for contact)",
+  },
+  {
+    type: "input",
+    name: "emailAddress",
+    message: "Please enter the your email address (for contact)",
+  },
+]);
 
 // Sets the README file contents
 let readmeDocument = `# ${title}  
@@ -94,16 +102,19 @@ ${contribution}
 ## Tests
 ${tests}
 ## Questions
+${renderQuestions(githubUsername, emailAddress)}
 `;
-// ${renderQuestions()}
 
-function renderQuestions() {
-    
+function renderQuestions(githubUsername, emailAddress) {
+  let questionsText = `For questions, issues or suggestions, please feel free to contact me on my GitHub, or drop me an email!  
+    - GitHub: https://github.com/${githubUsername}  
+    - Email Address: ${emailAddress}
+`;
+
+  return questionsText;
 }
 
-function displayBadges() {
-    
-}
+function displayBadges() {}
 
 fs.writeFile("./sample/README.md", readmeDocument);
 // console.log("success!");
---
+++ sample/README.md
@@ -0,0 +1,25 @@
+# klsjdflk  
+## Description
+sdfjkls  
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+## Installation
+sfdkfjs  
+## Usage
+dfsdkldjflsk
+## License
+mit
+## Contributing
+
+## Tests
+slkdfj
+## Questions
+For questions, issues or suggestions, please feel free to contact me on my GitHub, or drop me an email!  
+    - GitHub: https://github.com/cl-lee  
+    - Email Address: cl.lee.ofz@gmail.com
+
