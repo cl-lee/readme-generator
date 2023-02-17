90940416940213437dfba675f1d656c3b69b04c0
cl.lee <cl.lee.ofz@gmail.com>
2023-02-17 18:27:21 +0000

    added further user input items, added more content to template literal codes for setting README contents, named functions for displaying Questions section and displaying badges

--- index.mjs
+++ index.mjs
@@ -1,77 +1,81 @@
-import inquirer from 'inquirer';
-import fs from 'fs/promises';
+import inquirer from "inquirer";
+import fs from "fs/promises";
 // const path = require('path');
 // const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 // init();
 
-
 // ***PSEUDO CODES***
 // inquirer for user's input
-// inquirer 
-let {title, description, installation, usage, construction, test, license} = await inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: "What is the project's title?",
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: "Please enter a description of the project",
-        },
-        // {
-        //     type: 'input',
-        //     name: 'installation',
-        //     message: "Please enter the installation instructions",
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'usage',
-        //     message: "Please enter the usage instructions",
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'contribution',
-        //     message: "Please enter the contribution guidelines",
-        // },
-        // {
-        //     type: 'input',
-        //     name: 'test',
-        //     message: "Please enter the test instructions",
-        // },
-        {
-            type: 'checkbox',
-            name: "license",
-            message: "Select the application's license",
-            choices: [
-              { name: 'MIT', value: 'mit' },
-              { name: 'Mozilla Public License 2.0', value: 'mozilla' },
-            ],
-        },
-    ])
+// inquirer
+let { title, description, installation, usage, contribution, tests, license, github, emailAddress } =
+  await inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the project's title?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please enter a description of the project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Please enter the installation instructions",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Please enter the usage instructions",
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Please enter the contribution guidelines",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "Please enter the test instructions",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Select the application's license",
+        choices: [
+            { name: "MIT", value: "mit" },
+            { name: "Mozilla Public License 2.0", value: "mozilla" },
+        ],
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Please enter the your GitHub username (for contact)",
+    },
+    {
+      type: "input",
+      name: "emailAddress",
+      message: "Please enter the your email address (for contact)",
+    },
+
+  ]);
 
 // Sets the README file contents
-let readmeDocument = 
-`# ${title}
+let readmeDocument = `# ${title}  
 ## Description
-${description}
+${description}  
 ## Table of Contents
 - [Installation](#Installation)
 - [Usage](#Usage)
@@ -79,12 +83,29 @@ ${description}
 - [Contributing](#Contributing)
 - [Tests](#Tests)
 - [Questions](#Questions)
-`
+## Installation
+${installation}  
+## Usage
+${usage}
+## License
+${license}
+## Contributing
+${contribution}
+## Tests
+${tests}
+## Questions
+`;
+// ${renderQuestions()}
 
-fs.writeFile("./sample/SAMPLE.md", readmeDocument);
+function renderQuestions() {
+    
+}
+
+function displayBadges() {
+    
+}
+
+fs.writeFile("./sample/README.md", readmeDocument);
 // console.log("success!");
-
-
-
 
 // record screen for this app's readme!
